8fad9b43c6b9d2950ac47c16f38376f45966e5e7
Phil Leggetter <phil@leggetter.co.uk>
2023-04-12 14:35:21 +0100

    Update to use new MongoDB driver/promises

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -1,6 +1,6 @@
-
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -9,16 +9,15 @@ const client = new MongoClient(Db, {
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
+  connectToServer: async function (callback) {
+    try {
+      const db = await client.connect();
+      _db = db.db("employees");
+      console.log("Successfully connected to MongoDB.");
+      return callback();
+    } catch (err) {
       return callback(err);
-         });
+    }
   },
 
   getDb: function () {
--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -11,49 +11,40 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
 // This section will help you get a list of all the records.
-recordRoutes.route("/record").get(function (req, res) {
+recordRoutes.route("/record").get(async function (req, res) {
   let db_connect = dbo.getDb("employees");
-  db_connect
-    .collection("records")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
+  const result = await db_connect.collection("records").find({}).toArray();
+
+  return res.json(result);
 });
 
 // This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(function (req, res) {
+recordRoutes.route("/record/:id").get(async function (req, res) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect
-      .collection("records")
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);
-      });
+  let myquery = { _id: new ObjectId(req.params.id) };
+  const result = await db_connect.collection("records").findOne(myquery);
+
+  return res.json(result);
 });
 
 // This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, response) {
+recordRoutes.route("/record/add").post(async function (req, res) {
   let db_connect = dbo.getDb();
   let myobj = {
     name: req.body.name,
     position: req.body.position,
     level: req.body.level,
   };
-  db_connect.collection("records").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
+
+  const result = db_connect.collection("records").insertOne(myobj);
+  res.json(result);
 });
 
 // This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
+recordRoutes.route("/update/:id").post(async function (req, res) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let myquery = { _id: new ObjectId(req.params.id) };
   let newvalues = {
     $set: {
       name: req.body.name,
@@ -61,24 +52,21 @@ recordRoutes.route("/update/:id").post(function (req, response) {
       level: req.body.level,
     },
   };
-  db_connect
+  const result = await db_connect
     .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
+    .updateOne(myquery, newvalues);
+
+  console.log("1 document updated");
+  res.json(result);
 });
 
 // This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
+recordRoutes.route("/:id").delete(async (req, res) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
-  });
+  let myquery = { _id: new ObjectId(req.params.id) };
+  const result = await db_connect.collection("records").deleteOne(myquery);
+
+  res.json(result);
 });
 
 module.exports = recordRoutes;
