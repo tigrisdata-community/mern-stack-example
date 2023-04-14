7ee1f8bf1e36a788a26d635da1cd2bec27d4f6d8
Phil Leggetter <phil@leggetter.co.uk>
2023-04-14 11:22:39 +0100

    fix updateOne query

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -41,17 +41,17 @@ recordRoutes.route("/record/add").post(async function (req, res) {
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(async function (req, res) {
   let db_connect = dbo.getDb();
-  let myquery = { filter: { _id: req.params.id } };
-  let newvalues = {
-    $set: {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
+  let myquery = {
+    filter: { _id: req.params.id },
+    fields: {
+      $set: {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+      },
     },
   };
-  const result = await db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues);
+  const result = await db_connect.getCollection("records").updateOne(myquery);
 
   console.log("1 document updated");
   res.json(result);
